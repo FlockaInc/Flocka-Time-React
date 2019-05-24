7a6f5c2e7857c8bda6ddbd9ebf981af6ca44d4cb
Burke <burke1791@gmail.com>
2019-05-24 16:17:43 -0500

    ran a build

---
+++ build/precache-manifest.95245972e3115bdd6be63d783429f859.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a41a833ff6a2868b72b8e66a13791c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94f72c7ac8a97ad7d32b",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "d8f2d4ee182c3bb93426",
+    "url": "/static/css/main.bfdd67d6.chunk.css"
+  },
+  {
+    "revision": "94f72c7ac8a97ad7d32b",
+    "url": "/static/js/2.c85da353.chunk.js"
+  },
+  {
+    "revision": "d8f2d4ee182c3bb93426",
+    "url": "/static/js/main.4fba00c0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca862bdc7fe247f50a57391418e62e9d",
+    "url": "/static/media/flockback.ca862bdc.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
